06af343ecb8459b131dd93adfec9054b79106abb
Irfan Khan <irfan.ik227@gmail.com>
2018-01-05 13:08:44 +0500

    Completed

--- src/final.js
+++ src/final.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 
 import Tab from './App';
 
+
 class Final extends Component {
     
     render() {
         return (
             <div style={{marginTop: '5%'}}>
-                <h1 className='text-center'>SHARE YOUR CONTENT HERE</h1>
+                <h1 className='text-center'> <span className='text-info'> SHARE YOUR CONTENT</span> <span className='text-warning'>GLOBALLY</span></h1>
                 <div className='row'>
                     <div className='col-md-2'></div>
                     <div className='col-md-8'>
